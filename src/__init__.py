a6152f79ef3e16d23c0a3142f02ead3b3587949b
Prasham Vipul Prabhakar <prasham.pillu@gmail.com>
2022-11-13 12:05:16 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ import os
 
 db_details = {
     'username': 'root',
-    'password': 'potato',
+    'password': 'prasham28dec',
     'host': 'localhost',
     'database': 'blog_database',
 }
@@ -12,13 +12,14 @@ db_details = {
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://{username}:{password}@{server}/{db}'.format(
     username=db_details['username'], password=db_details['password'], server=db_details['host'], db=db_details['database'])
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # app.config["SECRET_KEY"]=os.environ.get('SECRET_KEY')
-app.config["DEBUG"]=True
+app.config["DEBUG"] = True
 
 # Flask features are inaccessible without a secret key
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 db = SQLAlchemy(app)
+
 from src import routes
